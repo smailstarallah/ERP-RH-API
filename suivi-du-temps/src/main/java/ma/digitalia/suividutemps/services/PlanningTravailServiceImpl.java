74cab3d3971528529f5a003355577c5b4e75b630
ismailstarallah <ismail.starallah@emsi-edu.ma>
2025-08-10 15:19:12 +0100

    Add initial application configuration and entity definitions
    - Create application.properties with application name and database configuration.
    - Define UserType and StatutPointage enums.
    - Implement repositories for RH, Pointage, and SoldeConge.
    - Add service interfaces for PlanningTravail and SoldeConge.
    - Create entity classes for Employe, Manager, and Users with necessary annotations.
    - Set up basic application structure with main classes for ERP-RH and related modules.

--- suivi-du-temps/src/main/java/ma/digitalia/suividutemps/services/PlanningTravailServiceImpl.java
+++ suivi-du-temps/src/main/java/ma/digitalia/suividutemps/services/PlanningTravailServiceImpl.java
@@ -1,6 +1,18 @@
 package ma.digitalia.suividutemps.services;
 
+import ma.digitalia.suividutemps.repositories.PlanningTravailRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlanningTravailServiceImpl implements PlanningTravailService {
+
+    PlanningTravailRepository planningTravailRepository;
+
+    public PlanningTravailServiceImpl(PlanningTravailRepository planningTravailRepository) {
+        this.planningTravailRepository = planningTravailRepository;
+    }
+
+
     @Override
     public void startTimeTracking(Long empId) {
 
